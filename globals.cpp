be83243383d270a24cbdb76dece00a1e148d5817
Ohohcakester <ohohgit@gmail.com>
2015-12-05 15:17:54 +0800

    readme

--- globals.cpp
+++ globals.cpp
@@ -10,7 +10,7 @@ int RES_X = 800;
 int RES_Y = 600;
 int TILE_WIDTH = 50;
 
-sf::RenderWindow window(sf::VideoMode(RES_X, RES_Y), "SFML In Space");
+sf::RenderWindow window(sf::VideoMode(RES_X, RES_Y), "Maze of Dat");
 
 Textures::Textures() {
     if (!background.loadFromFile("img/bg.jpg")) {
